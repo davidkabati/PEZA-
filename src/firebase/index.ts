4abbb4b7d438a92f50dd38b94b68cbad61cf9797
Jacques Mac <mac@Jacques-MacBook-Pro.local>
2021-08-02 14:25:54 +0100

    feat: remove expo constants for env

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -2,17 +2,15 @@
 import firebase from 'firebase';
 import 'firebase/firestore';
 
-import Constants from 'expo-constants';
-
 export default () => {
   // Initialize Firebase
   const firebaseConfig = {
-    apiKey: Constants.manifest.extra!.API_KEY,
-    authDomain: Constants.manifest.extra!.AUTH_DOMAIN,
-    databaseURL: Constants.manifest.extra!.DATABASE_URL,
-    projectId: Constants.manifest.extra!.PROJECT_ID,
-    storageBucket: Constants.manifest.extra!.STORAGE_BUCKET,
-    messagingSenderId: Constants.manifest.extra!.MESSAGING_SENDER_ID,
+    apiKey: '"AIzaSyAqg9hS0qLUrJ-WePZV08UnEnVM_vxHLqY"',
+    authDomain: 'peza-mobile-app.firebaseapp.com',
+    databaseURL: 'https://peza-mobile-app-default-rtdb.firebaseio.com/',
+    projectId: 'peza-mobile-app',
+    storageBucket: 'peza-mobile-app.appspot.com',
+    messagingSenderId: '736624304188',
   };
 
   if (firebase.apps.length === 0) {
